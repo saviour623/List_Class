308bd7d2c3de18de5c321ba833919af570bdf81a
saviour623 <saviourme33@gmail.com>
2023-09-29 04:38:49 +0100

    added parenthesis check to array_args

--- test/test.c
+++ test/test.c
@@ -12,8 +12,8 @@ static void check(char *obj, int type, void *p){
 }
 int main(void){
   //cT for a C type, cP for pointer, cG for group C type and gP for group pointer,
-  list(cT)((mylist), 'a');
-
+  list(cT)((mylist), ('a'), -);
+  //ARRAY(func, obj, memtype, "hello")
   return 0;
 }
 //TODO: set a flag for group and a flag for single, so that when ever group is used, single cannot be used
